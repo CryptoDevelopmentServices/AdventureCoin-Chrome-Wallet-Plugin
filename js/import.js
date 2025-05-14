61be3972d657de2a83c792373e53008509989d8e
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-05-14 20:25:44 +0100

    1st push

---
+++ js/import.js
@@ -0,0 +1,323 @@
+var netconfig
+var href
+window.onload = function() {
+    var getaddress = localStorage.getItem("address")
+    // Set overlay.js to open to import page
+    localStorage.setItem("opened", "import.html")
+
+    setImportLang()
+
+    var apiget = localStorage.getItem("apiSet")
+
+    // Sets History Tab to open to explorer
+    if (apiget == "mainnet" || apiget == null) {
+        href = "http://explorer.adventurecoin.quest/#/address/" + getaddress
+    }
+    $("#history").attr("href", href)
+
+    getImportAPI()
+}
+
+function getImportAPI() {
+    // Set Network config according to Endpoint selection
+    if (localStorage.getItem("api") == "http://157.180.76.198:4321" || localStorage.getItem("api") == null){
+        netconfig = {					
+           'network': {
+                'messagePrefix': '\x19AdventureCoin Signed Message:\n',
+                'bip32': {
+                    'public': 0x0488b21e,
+                    'private': 0x0488ade4
+                },
+               'bech32': 'advc',
+               'pubKeyHash': 0x17,
+			   'scriptHash': 0x0A,
+			   'wif': 0x7B}
+        }
+    }
+    
+}
+
+var alert1
+var alert2
+$("#wifImport").click(function() {
+    // If the WIF Key is 51 or 52 character length, run import function
+    if ($("#wifInput").val().length == 52 || $("#wifInput").val().length == 51) {
+
+        // Get WIF from the user input
+        var wifInput = $("#wifInput").val()
+
+        var wifKey = bitcoin.ECPair.fromWIF(wifInput, netconfig['network'])
+        var redeem = bitcoin.payments.p2wpkh({'pubkey': wifKey.publicKey, 'network': netconfig['network']})
+    
+        var legacyadd = bitcoin.payments.p2pkh({'pubkey': wifKey.publicKey, 'network': netconfig['network']}).address
+        var segwitadd = bitcoin.payments.p2sh({'redeem': redeem, 'network': netconfig['network']}).address
+        var bech32add = bitcoin.payments.p2wpkh({'pubkey': wifKey.publicKey, 'network': netconfig['network']}).address
+
+        alert(alert1)
+
+        // Only set bech32 address and WIF key
+        localStorage.setItem("address", bech32add)
+        localStorage.setItem("wifKey", $("#wifInput").val())
+    
+        $("#showLegacy").text(legacyadd)
+        $("#showBech32").text(bech32add)
+        $("#showSegwit").text(segwitadd)
+    }
+    // Else show error
+    else if ($("#wifInput").val().length != 52 || $("#wifInput").val().length != 51){
+        alert(alert2)
+
+        $("#showLegacy").text('')
+        $("#showBech32").text('')
+        $("#showSegwit").text('')
+    }
+
+    var getaddress = localStorage.getItem("address")
+
+    var apiget = localStorage.getItem("apiSet")
+
+    // Sets History Tab to open to explorer
+    if (apiget == "mainnet") {
+        href = "http://explorer.adventurecoin.quest/#/address/" + getaddress
+    }
+})
+
+var lang = {
+    'en': {
+        // Page text
+        'wifImport': "Import WIF",
+        'legacy': "Legacy address:",
+        'segwit': "Segwit address:",
+        'bech32': "Bech32 address:",
+        'alert1': "WIF Imported Successfully",
+        "alert2": "WIF Invalid",
+        'logoutreminder': {
+            'part1': "Remember to",
+            'logoutlink': "Logout",
+            'part2': "before exiting Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "Create Wallet",
+        'import-wallet': "Import Wallet",
+        'your-wallet': "Your Wallet",
+        'send': "Send",
+        'tx-history': "History",
+        'chain-info': "Chain Info",
+        'settings': "Settings"
+    },
+    
+
+    'fr': {
+        // Page text
+        'wifImport': "Importer WIF",
+        'legacy': "Legacy Adresse:",
+        'segwit': "Segwit Adresse:",
+        'bech32': "Bech32 Adresse:",
+        'alert1': "WIF Importé Succès",
+        "alert2": "WIF invalide",
+        'logoutreminder': {
+            'part1': "Se souvenir de",
+            'logoutlink': "Se Déconnecter",
+            'part2': "avant de sortir Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "Créer Portefeuille",
+        'import-wallet': "Importer Portefeuille",
+        'your-wallet': "Votre Portefeuille",
+        'send': "Envoyer",
+        'tx-history': "L'histoire",
+        'chain-info': "Chaîne Données",
+        'settings': "Paramètres"        
+    },
+
+    'kr': {
+        // Page text
+        'wifImport': "수입 WIF",
+        'legacy': "Legacy 주소를:",
+        'segwit': "Segwit 주소를:",
+        'bech32': "Bech32 주소를:",
+        'alert1': "WIF 수입품 성공적으로",
+        "alert2": "WIF 무효로 하는",
+        'logoutreminder': {
+            'part1': "기억해",
+            'logoutlink': "로그 아웃",
+            'part2': "종료하기 전에",
+        },
+
+        // Tab text
+        'create-wallet': "창조하다 지갑",
+        'import-wallet': "수입 지갑",
+        'your-wallet': "너의 지갑",
+        'send': "보내다",
+        'tx-history': "역사",
+        'chain-info': "체인 정보",
+        'settings': "설정"
+    },
+
+    'id': {
+        // Page text
+        'wifImport': "Impor WIF",
+        'legacy': "Legacy Alamat:",
+        'segwit': "Segwit Alamat:",
+        'bech32': "Bech32 Alamat:",
+        'alert1': "WIF yg diimpor Berhasil",
+        "alert2": "WIF batal",
+        'logoutreminder': {
+            'part1': "Ingat untuk",
+            'logoutlink': "Keluar",
+            'part2': "sebelum keluar Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "Membuat Dompet",
+        'import-wallet': "Impor Dompet",
+        'your-wallet': "Dompet Anda",
+        'send': "Kirim",
+        'tx-history': "Riwayat",
+        'chain-info': "Data Rantai",
+        'settings': "Pengaturan"
+    },
+
+    'es': {
+        // Page text
+        'wifImport': "Importar WIF",
+        'legacy': "Legacy Alocución:",
+        'segwit': "Segwit Alocución:",
+        'bech32': "Bech32 Alocución:",
+        'alert1': "WIF Importado Exitosamente",
+        "alert2": "WIF inválido",
+        'logoutreminder': {
+            'part1': "Recuerda a",
+            'logoutlink': "Cerrar sesión",
+            'part2': "antes de irse Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "Billetera Crear",
+        'import-wallet': "Billetera Importar",
+        'your-wallet': "Billetera Tu",
+        'send': "Enviar",
+        'tx-history': "Historia",
+        'chain-info': "Informacion Red",
+        'settings': "El Ajuste"
+    },
+
+    'ru': {
+        // Page text
+        'wifImport': "Импорт WIF ключа",
+        'legacy': "Legacy адрес:",
+        'segwit': "Segwit адрес:",
+        'bech32': "Bech32 адрес:",
+        'alert1': "WIF ключ успешно импортирован",
+        "alert2": "Недействительный WIF",
+        'logoutreminder': {
+            'part1': "Помните в",
+            'logoutlink': "Выйти",
+            'part2': "перед выходом",
+        },
+    
+        // Tab text
+        'create-wallet': "Создать кошелек",
+        'import-wallet': "Импорт кошелька",
+        'your-wallet': "Ваш кошелек",
+        'send': "Отправить",
+        'tx-history': "История",
+        'chain-info': "Информация о сети",
+        'settings': "Настройки"
+    },
+
+    'zh': {
+        // Page text
+        'wifImport': "导入WIF",
+        'legacy': "Legacy地址:",
+        'segwit': "Segwit地址:",
+        'bech32': "Bech32地址:",
+        'alert1': "导入WIF地址成功",
+        "alert2": "无效的WIF",
+        'logoutreminder': {
+            'part1': "记得 至",
+            'logoutlink': "登出",
+            'part2': "退出前 Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "创建钱包",
+        'import-wallet': "导入钱包",
+        'your-wallet': "你的钱包",
+        'send': "发送",
+        'tx-history': "历史",
+        'chain-info': "网络信息",
+        'settings': "设置"
+    },
+
+    'ja': {
+        // Page text
+        'wifImport': "インポート WIF",
+        'legacy': "Legacy 住所:",
+        'segwit': "Segwit 住所:",
+        'bech32': "Bech32 住所:",
+        'alert1': "WIF 舶来 成功した",
+        "alert2": "WIF 病弱な",
+        'logoutreminder': {
+            'part1': "覚えて に",
+            'logoutlink': "ログアウト",
+            'part2': "出る前に Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "作成する 財布",
+        'import-wallet': "インポート 財布",
+        'your-wallet': "きみの 財布",
+        'send': "送る",
+        'tx-history': "歴史",
+        'chain-info': "通信網 情報",
+        'settings': "設定"
+    },
+}
+
+function setImportLang() {
+    if (localStorage['lang'] == null) {
+        // Page text
+        $("#segwit").text(lang['en']['segwit'])
+        $("#legacy").text(lang['en']['legacy'])
+        $("#wifImport").text(lang['en']['wifImport'])
+        $("#bech32").text(lang['en']['bech32'])
+        $("#part1").text(lang['en']['logoutreminder']['part1'])
+        $("#logoutlink").text(lang['en']['logoutreminder']['logoutlink'])
+        $("#part2").text(lang['en']['logoutreminder']['part2'])
+        alert1 = lang['en']['alert1']
+        alert2 = lang['en']['alert2']
+
+        // Tab text
+        $("#create-wallet").text(lang['en']['create-wallet'])
+        $("#import-wallet").text(lang['en']['import-wallet'])
+        $("#your-wallet").text(lang['en']['your-wallet'])
+        $("#send").text(lang['en']['send'])
+        $("#tx-history").text(lang['en']['tx-history'])
+        $("#chain-info").text(lang['en']['chain-info'])
+        $("#settings").text(lang['en']['settings'])
+    }
+    else {
+        // Page text
+        $("#segwit").text(lang[localStorage.getItem("lang")]['segwit'])
+        $("#legacy").text(lang[localStorage.getItem("lang")]['legacy'])
+        $("#wifImport").text(lang[localStorage.getItem("lang")]['wifImport'])
+        $("#bech32").text(lang[localStorage.getItem("lang")]['bech32'])
+        $("#part1").text(lang[localStorage.getItem("lang")]['logoutreminder']['part1'])
+        $("#logoutlink").text(lang[localStorage.getItem("lang")]['logoutreminder']['logoutlink'])
+        $("#part2").text(lang[localStorage.getItem("lang")]['logoutreminder']['part2'])
+        alert1 = lang[localStorage.getItem("lang")]['alert1']
+        alert2 = lang[localStorage.getItem("lang")]['alert2']
+
+        // Tab text
+        $("#create-wallet").text(lang[localStorage.getItem("lang")]['create-wallet'])
+        $("#import-wallet").text(lang[localStorage.getItem("lang")]['import-wallet'])
+        $("#your-wallet").text(lang[localStorage.getItem("lang")]['your-wallet'])
+        $("#send").text(lang[localStorage.getItem("lang")]['send'])
+        $("#tx-history").text(lang[localStorage.getItem("lang")]['tx-history'])
+        $("#chain-info").text(lang[localStorage.getItem("lang")]['chain-info'])
+        $("#settings").text(lang[localStorage.getItem("lang")]['settings'])
+    }
+}
---
+++ js/send.js
@@ -0,0 +1,499 @@
+var wif
+var wifKey
+var api
+var address
+var netconfig
+var href
+window.onload = function() {
+    // Get WIF and address from local storage
+    wifKey = localStorage.getItem("wifKey")
+    address = localStorage.getItem("address")
+
+    // Define variable to set placeholder if user chooses mainnet or testnet
+    var inputPlaceholder = $("#sendInput")
+
+    // Set overlay.js to open to send page
+    localStorage.setItem("opened", "send.html")
+
+    setSendPageLang()
+
+    apiget = localStorage.getItem("apiSet")
+
+    // Set history page to open to explorer & sets placeholder to testnet or mainnet prefix
+    if (apiget == "mainnet" || apiget == null) {
+        api = "http://157.180.76.198:4321"
+        inputPlaceholder.attr("placeholder", "advc1q...")
+        href = "http://explorer.adventurecoin.quest/#/address/" + address
+    }
+    $("#history").attr("href", href)
+
+    getSendAPI()
+}
+
+var errororsuccess
+function getSendAPI() {
+    // Set Network config according to Endpoint selection
+    if (localStorage.getItem("api") == "http://157.180.76.198:4321" || localStorage.getItem("api") == null){
+        netconfig = {					
+           'network': {
+                'messagePrefix': '\x19AdventureCoin Signed Message:\n',
+                'bip32': {
+                    'public': 0x0488b21e,
+                    'private': 0x0488ade4
+                },
+               'bech32': 'advc',
+               'pubKeyHash': 0x17,
+			   'scriptHash': 0x0A,
+			   'wif': 0x7B}
+        }
+    }
+
+}
+
+$("#sendTx").click(function () {
+    var feeinput = document.getElementById("feeAdvc")
+    console.log(feeinput.value)
+    var fee = undefined
+    var feeShow = undefined
+    if (feeinput && feeinput.value) {
+        fee = convertAmountFormat(feeinput.value, true)
+        feeShow = convertAmountFormat(fee)
+    }
+    else {
+        fee = 1000
+        feeShow = convertAmountFormat(fee)
+    }
+    // Don't put fee in convertion of amount format
+    var amount = convertAmountFormat(parseFloat($("#amountADVC").val()), true) + fee
+    var amountShow = convertAmountFormat(amount)
+    var receiver = $("#sendInput").val()
+
+    var scripts = []
+
+    ask = confirm("Confirm Transaction. You are about to send " + $("#amountADVC").val() + " ADVC to " + receiver + ". The fee is " + feeShow/*(amountShow - Number($("#amountADVC").val()))*/ + " ADVC\nTotal Cost: " + amountShow + " ADVC")
+    if (ask == true){
+        var showErrororSuccess = $("#showErrororSuccess")
+        showErrororSuccess.text("Sending Transaction...")
+
+        wif = bitcoin.ECPair.fromWIF(wifKey, netconfig['network'])
+
+        // Get unspent values to calculate change and sending value
+        Promise.resolve($.ajax({
+            url: api + "/unspent/" + address + "?amount=" + parseInt(amount, 10),
+            dataType: 'json',
+            type: 'GET'
+        })).then(function(data) {
+
+            var txbuilder = new bitcoin.TransactionBuilder(netconfig['network'])
+            txbuilder.setVersion(2)
+
+            txbuilder.addOutput(receiver, (amount - fee))
+            
+            var txvalue = 0
+            for (var i = 0, size = data.result.length; i < size; i++) {
+                var prevtxid = data.result[i].txid
+                var txindex = data.result[i].index
+                txvalue += data.result[i].value
+
+                var script = bitcoin.Buffer(data.result[i].script, 'hex')
+                var typeofaddress = scriptType(script)
+
+                if (typeofaddress == 'bech32') {
+                    var bech32script = bitcoin.payments.p2wpkh({'pubkey': wif.publicKey, 'network': netconfig['network']})
+
+                    txbuilder.addInput(prevtxid, txindex, null, bech32script.output)
+                }
+
+                else {
+                    txbuilder.addInput(prevtxid, txindex)
+                }
+
+                scripts.push({'script': script, 'type': typeofaddress, 'value': data.result[i].value})
+            }
+
+            if (txvalue >= amount) {
+                var txchange = txvalue - amount
+                // If the change is greater than 0, send the change back to the sender
+                if (txchange > 0) {
+                    // Send change back to sender
+                    txbuilder.addOutput(address, txchange)
+                }
+
+                for (var i = 0, size = scripts.length; i < size; i++){
+                    switch (scripts[i].type) {
+                        case 'bech32':
+                            var value = scripts[i].value
+                            txbuilder.sign(i, wif, null, null, value, null)
+                            break
+                        
+                        case 'segwit':
+                            var value = scripts[i].value
+                            var redeem = bitcoin.payments.p2wpkh({'pubkey': wif.publicKey, 'network': netconfig['network']})
+                            var segwitscript = bitcoin.payments.p2sh({'redeem': redeem, 'network': netconfig['network']})
+
+                            txbuilder.sign(i, wif, segwitscript.output, null, value, null)
+                            break
+                        
+                        case 'legacy':
+                            txbuilder.sign(i, wif)
+                            break
+                        
+                        default:
+                            showErrororSuccess.text("Bad UTXO")
+                    }
+                }
+                var txfinal = txbuilder.build()
+                
+                // Broadcast the transaction to the network
+                Promise.resolve($.ajax({
+                    'url': api + '/broadcast',
+                    'method': 'POST',
+                    'data': {
+                        'raw': txfinal.toHex()
+                    }
+                })).then(function(data) {
+                    if (data.error == null) {
+                        // Success message according to language set
+                        showErrororSuccess.text(errororsuccess['success'] + data.result)
+                    }
+
+                    else {
+                        // Broadcast error according to language set
+                        showErrororSuccess.text(errororsuccess['error']['broadcast'] + data.error.message)
+                    }
+
+                    resetForm()
+                })
+                
+            }
+
+            else {
+                // Fund error according to language set
+                showErrororSuccess.text(errororsuccess['error']['funds'])
+            }
+        })
+    }
+    else {
+        // User transaction canceled according to language set
+        var showErrororSuccess = $("#showErrororSuccess")
+        showErrororSuccess.text(errororsuccess['error']['cancel'])
+    }
+})
+
+// Get type of address given the script hash
+function scriptType(script) {
+    var type = undefined
+
+    if (script[0] == bitcoin.opcodes.OP_0 && script[1] == 20) {
+        type = 'bech32'
+    }
+
+    if (script[0] == bitcoin.opcodes.OP_HASH160 && script[1] == 20) {
+        type = 'segwit'
+    }
+
+    if (script[0] == bitcoin.opcodes.OP_DUP && script[1] == bitcoin.opcodes.OP_HASH160 && script[2] == 20) {
+        type = 'legacy'
+    }
+
+    return type
+
+}
+
+// Reset the values after user sends
+function resetForm() {
+    $("#amountADVC").val('')
+    $("#sendInput").val('')
+    $("#feeAdvc").val('')
+}
+
+// Conversion of standars integer to satoshis
+function convertAmountFormat(amount, invert = false) {
+    decimals = 8
+    if (!invert) {
+        return parseFloat((amount / Math.pow(10, decimals)).toFixed(decimals))
+    }
+    else {
+        return parseInt(amount * Math.pow(10, decimals))
+    }
+}
+
+// Multi Lang
+var lang = {
+    'en': {
+        // Page text
+        'send-to': "Send To: ",
+        'amount-advc': "Amount: ",
+        'sendTx': "Send Transaction",
+        'showErrororSuccess': {
+            'success': "Success! Transaction ID: ",
+            'error': {
+                'broadcast': "Broadcast Failed! Error: ",
+                'funds': "Error: Not enough funds",
+                'cancel': "Transaction Cancelled",
+            },
+        },
+        'logoutreminder': {
+            'part1': "Remember to",
+            'logoutlink': "Logout",
+            'part2': "before exiting Chrome",
+        },
+
+
+        // Tab text
+        'create-wallet': "Create Wallet",
+        'import-wallet': "Import Wallet",
+        'your-wallet': "Your Wallet",
+        'send': "Send",
+        'tx-history': "History",
+        'chain-info': "Chain Info",
+        'settings': "Settings"
+    },
+    
+
+    'fr': {
+        // Page text
+        'send-to': "Envoyer pour: ",
+        'amount-advc': "Quantité: ",
+        'sendTx': "Envoyer Transaction",
+        'showErrororSuccess': {
+            'success': "Succès! Transaction ID: ",
+            'error': {
+                'broadcast': "Diffuser Échoué! Erreur: ",
+                'funds': "Erreur: Pas assez fonds",
+                'cancel': "Transaction Annulé",
+            },
+        },
+        'logoutreminder': {
+            'part1': "Se souvenir de",
+            'logoutlink': "Se Déconnecter",
+            'part2': "avant de sortir Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "Créer Portefeuille",
+        'import-wallet': "Importer Portefeuille",
+        'your-wallet': "Votre Portefeuille",
+        'send': "Envoyer",
+        'tx-history': "L'histoire",
+        'chain-info': "Chaîne Données",
+        'settings': "Paramètres"        
+    },
+
+    'kr': {
+        // Page text
+        'send-to': "보내다 에게: ",
+        'amount-advc': "양: ",
+        'sendTx': "보내다 트랜잭션",
+        'showErrororSuccess': {
+            'success': "성공! 트랜잭션 ID: ",
+            'error': {
+                'broadcast': "방송 실패한! 오류: ",
+                'funds': "오류: 부족한 자금",
+                'cancel': "트랜잭션 취소 된",
+            },
+        },
+        'logoutreminder': {
+            'part1': "기억해",
+            'logoutlink': "로그 아웃",
+            'part2': "종료하기 전에",
+        },
+
+        // Tab text
+        'create-wallet': "창조하다 지갑",
+        'import-wallet': "수입 지갑",
+        'your-wallet': "너의 지갑",
+        'send': "보내다",
+        'tx-history': "역사",
+        'chain-info': "체인 정보",
+        'settings': "설정"
+    },
+
+    'id': {
+        // Page text
+        'send-to': "Kirim Untuk: ",
+        'amount-advc': "Jumlahnya: ",
+        'sendTx': "Kirim Transaksi",
+        'showErrororSuccess': {
+            'success': "Keberhasilan! Transaksi ID: ",
+            'error': {
+                'broadcast': "Siaran Gagal! Kesalahan: ",
+                'funds': "Kesalahan: Tidak cukup dana",
+                'cancel': "Transaksi Dibatalkan",
+            },
+        },
+        'logoutreminder': {
+            'part1': "Ingat untuk",
+            'logoutlink': "Keluar",
+            'part2': "sebelum keluar Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "Membuat Dompet",
+        'import-wallet': "Impor Dompet",
+        'your-wallet': "Dompet Anda",
+        'send': "Kirim",
+        'tx-history': "Riwayat",
+        'chain-info': "Data Rantai",
+        'settings': "Pengaturan"
+    },
+
+    'es': {
+        // Page text
+        'send-to': "Enviar a: ",
+        'amount-advc': "Cantidad: ",
+        'sendTx': "Enviar Transacción",
+        'showErrororSuccess': {
+            'success': "Éxito! Transacción ID: ",
+            'error': {
+                'broadcast': "Transmitir Fracasado! Error: ",
+                'funds': "Error: No es Suficiente fondos",
+                'cancel': "Transacción Anulado",
+            },
+        },
+        'logoutreminder': {
+            'part1': "Recuerda a",
+            'logoutlink': "Cerrar sesión",
+            'part2': "antes de irse Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "Billetera Crear",
+        'import-wallet': "Billetera Importar",
+        'your-wallet': "Billetera Tu",
+        'send': "Enviar",
+        'tx-history': "Historia",
+        'chain-info': "Informacion Red",
+        'settings': "El Ajuste"
+    },
+
+    'ru': {
+        // Page text
+        'send-to': "Отправить к: ",
+        'amount-advc': "Сумма: ",
+        'sendTx': "Отправить",
+        'showErrororSuccess': {
+            'success': "Транзакция успешно отправлена: ",
+            'error': {
+                'broadcast': "Ошибка передачи: ",
+                'funds': "Ошибка: недостаточный баланс",
+                'cancel': "Отменить",
+            },
+        },
+        'logoutreminder': {
+            'part1': "Помните в",
+            'logoutlink': "Выйти",
+            'part2': "перед выходом",
+        },
+
+        // Tab text
+        'create-wallet': "Создать кошелек",
+        'import-wallet': "Импорт кошелька",
+        'your-wallet': "Ваш кошелек",
+        'send': "Отправить",
+        'tx-history': "История",
+        'chain-info': "Информация о сети",
+        'settings': "Настройки"
+    },
+
+    'zh': {
+        // Page text
+        'send-to': "发送至: ",
+        'amount-advc': "数量: ",
+        'sendTx': "发送交易",
+        'showErrororSuccess': {
+            'success': "成功! 交易 ID: ",
+            'error': {
+                'broadcast': "广播失败! 错误: ",
+                'funds': "错误: 资金不足",
+                'cancel': "交易已取消",
+            },
+        },
+        'logoutreminder': {
+            'part1': "记得 至",
+            'logoutlink': "登出",
+            'part2': "退出前 Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "创建钱包",
+        'import-wallet': "导入钱包",
+        'your-wallet': "你的钱包",
+        'send': "发送",
+        'tx-history': "历史信息",
+        'chain-info': "网络信息",
+        'settings': "设置"
+    },
+
+    'ja': {
+        // Page text
+        'send-to': "に 送る: ",
+        'amount-advc': "量: ",
+        'sendTx': "送る トランザクション",
+        'showErrororSuccess': {
+            'success': "成功! トランザクション ID: ",
+            'error': {
+                'broadcast': "放送 失敗! エラー: ",
+                'funds': "エラー: 十分ではない 資金",
+                'cancel': "トランザクション キャンセル",
+            },
+        },
+        'logoutreminder': {
+            'part1': "覚えて に",
+            'logoutlink': "ログアウト",
+            'part2': "出る前に Chrome",
+        },
+
+        // Tab text
+        'create-wallet': "作成する 財布",
+        'import-wallet': "インポート 財布",
+        'your-wallet': "きみの 財布",
+        'send': "送る",
+        'tx-history': "歴史",
+        'chain-info': "通信網 情報",
+        'settings': "設定"
+    },
+}
+
+// Set Multi Lang
+function setSendPageLang() {
+    if (localStorage['lang'] == null) {
+        // Page Text
+        $("#send-to").text(lang['en']['send-to'])
+        $("#amount-advc").text(lang['en']['amount-advc'])
+        $("#sendTx").text(lang['en']['sendTx'])
+        $("#part1").text(lang['en']['logoutreminder']['part1'])
+        $("#logoutlink").text(lang['en']['logoutreminder']['logoutlink'])
+        $("#part2").text(lang['en']['logoutreminder']['part2'])
+        errororsuccess = lang['en']['showErrororSuccess']
+
+        // Tab text
+        $("#create-wallet").text(lang['en']['create-wallet'])
+        $("#import-wallet").text(lang['en']['import-wallet'])
+        $("#your-wallet").text(lang['en']['your-wallet'])
+        $("#send").text(lang['en']['send'])
+        $("#tx-history").text(lang['en']['tx-history'])
+        $("#chain-info").text(lang['en']['chain-info'])
+        $("#settings").text(lang['en']['settings'])        
+    }
+    
+    else {
+        // Page Text
+        $("#send-to").text(lang[localStorage.getItem("lang")]['send-to'])
+        $("#amount-advc").text(lang[localStorage.getItem("lang")]['amount-advc'])
+        $("#sendTx").text(lang[localStorage.getItem("lang")]['sendTx'])
+        $("#part1").text(lang[localStorage.getItem("lang")]['logoutreminder']['part1'])
+        $("#logoutlink").text(lang[localStorage.getItem("lang")]['logoutreminder']['logoutlink'])
+        $("#part2").text(lang[localStorage.getItem("lang")]['logoutreminder']['part2'])
+        errororsuccess = lang[localStorage.getItem("lang")]['showErrororSuccess']
+
+        // Tab text
+        $("#create-wallet").text(lang[localStorage.getItem("lang")]['create-wallet'])
+        $("#import-wallet").text(lang[localStorage.getItem("lang")]['import-wallet'])
+        $("#your-wallet").text(lang[localStorage.getItem("lang")]['your-wallet'])
+        $("#send").text(lang[localStorage.getItem("lang")]['send'])
+        $("#tx-history").text(lang[localStorage.getItem("lang")]['tx-history'])
+        $("#chain-info").text(lang[localStorage.getItem("lang")]['chain-info'])
+        $("#settings").text(lang[localStorage.getItem("lang")]['settings'])        
+    }
+}
